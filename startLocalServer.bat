7cecdecd050421eaba367fef7d7513e4a3ea826e
MarianaMikhael <marianamikhael@live.com>
2020-05-20 22:49:45 -0300

    Configurações iniciais do projeto

---
+++ startLocalServer.bat
@@ -0,0 +1,4 @@
+cd C:\Users\W10.DESKTOP-M803K14\Desktop\OPE-ALMSolutions\venv\Scripts
+call activate.bat
+cd C:\Users\W10.DESKTOP-M803K14\Desktop\OPE-ALMSolutions\ValeVerdeEventos
+python manage.py runserver
