2ee8d46e110d036b5a2773c243b4ab05eb028ebf
MarianaMikhael <marianamikhael@live.com>
2020-05-22 23:47:08 -0300

    Criação do ModelForm

--- ValeVerdeEventos/calendario/admin.py
+++ ValeVerdeEventos/calendario/admin.py
@@ -2,8 +2,8 @@ from django.contrib import admin
 
 # Register your models here.
 
-from calendario.models import clientes, eventos, fornecedores
+from calendario.models import clientes, fornecedores, eventos
 
 admin.site.register(clientes.t_cliente),
-admin.site.register(eventos.t_evento),
 admin.site.register(fornecedores.t_fornecedor),
+admin.site.register(eventos.t_evento),
