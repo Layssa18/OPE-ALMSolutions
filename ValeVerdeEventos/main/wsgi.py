b3defac29eeec01f3e0ea3ba7c97a1c23d444742
MarianaMikhael <marianamikhael@live.com>
2020-05-23 17:02:16 -0300

    Ajuste de Bug

--- ValeVerdeEventos/main/wsgi.py
+++ ValeVerdeEventos/main/wsgi.py
@@ -8,11 +8,17 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
 import os
+import signal
+import sys
+import traceback
+import time
 
 from django.core.wsgi import get_wsgi_application
 
 from dj_static import Cling
+from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main.settings')
 
 application = Cling(get_wsgi_application())
+application = DjangoWhiteNoise(application)
