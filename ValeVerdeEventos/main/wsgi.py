88713aee40f1439c90335435931189096d959075
MarianaMikhael <marianamikhael@live.com>
2020-05-23 16:28:58 -0300

    Ajuste de Bug

--- ValeVerdeEventos/main/wsgi.py
+++ ValeVerdeEventos/main/wsgi.py
@@ -13,6 +13,6 @@ from django.core.wsgi import get_wsgi_application
 
 from dj_static import Cling
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ValeVerdeEventos.main.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main.settings')
 
 application = Cling(get_wsgi_application())
