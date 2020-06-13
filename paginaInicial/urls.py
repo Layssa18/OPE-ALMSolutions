ec88002256616fb0d7235925ec35488371b56b9f
Alex Jones <alex.silva@aluno.faculdadeimpacta.com.br>
2020-06-13 03:15:37 -0300

    primeira integração com API Read dos proximos 10 eventos

--- paginaInicial/urls.py
+++ paginaInicial/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import index
+from .views import home, index
 
 urlpatterns = [
     path('', index, name='index'),
+    path('agendaapi/', home , name='agendaapi')
 ]
